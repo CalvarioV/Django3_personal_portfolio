2e25557207a6ef4c99a12c0ad2751c56d678d06f
CalvarioV <calvario24@icloud.com>
2021-07-16 08:23:49 -0700

    new views and pages and css

--- bestguide/urls.py
+++ bestguide/urls.py
@@ -5,6 +5,7 @@ app_name = 'bestguide'
 
 urlpatterns = [
     path('', views.besthome, name='besthome'),
+    path('testing', views.testing, name='testing'),
 
 
 ]
--- bestguide/views.py
+++ bestguide/views.py
@@ -4,3 +4,7 @@ from django.shortcuts import render
 # Create your views here.
 def besthome(request):
     return render(request, 'bestguide/besthome.html')
+
+
+def testing(request):
+    return render(request, 'bestguide/testing.html')
