399561db120850e65015ac4eb0853970d2c54040
CalvarioV <calvario24@icloud.com>
2024-07-09 13:10:47 -0700

    loading icon added

---
+++ portfolio/static/portfolio/js/mainpage.js
@@ -0,0 +1,20 @@
+// script.js
+window.addEventListener('load', function() {
+  var loader = document.getElementById('loader');
+  var minimumTimeElapsed = false;
+  var pageFullyLoaded = false;
+
+  // Set a timeout for 3 seconds
+  setTimeout(function() {
+    minimumTimeElapsed = true;
+    if (pageFullyLoaded) {
+      loader.style.display = 'none';
+    }
+  }, 3000);
+
+  // Check if page is fully loaded
+  pageFullyLoaded = true;
+  if (minimumTimeElapsed) {
+    loader.style.display = 'none';
+  }
+});
