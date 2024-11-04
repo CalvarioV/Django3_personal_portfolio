469f4a2c3ec25060006081692255b06a0bc0da11
CalvarioV <calvario24@icloud.com>
2024-11-03 23:09:29 -0800

    resume image fix

--- portfolio/static/portfolio/js/resume1.js
+++ portfolio/static/portfolio/js/resume1.js
@@ -1,3 +1,28 @@
+const darkModeBtn = document.getElementById("dark-mode-btn");
+const themeImage = document.getElementById("themeImage");
+
+darkModeBtn.addEventListener("click", function () {
+  document.body.classList.toggle("dark-mode");
+
+  // Fade out the image
+  themeImage.style.opacity = 0;
+
+  // Wait for the fade-out to complete before changing the src
+  setTimeout(() => {
+    if (document.body.classList.contains("dark-mode")) {
+      themeImage.src = "https://calvariov.pythonanywhere.com/site/subfolder/calvario1Oct23.png"; // Image for dark mode
+    } else {
+      themeImage.src = "https://calvariov.pythonanywhere.com/site/subfolder/myquotecvlogoFinal.jpg"; // Image for light mode
+    }
+
+    // Fade the image back in after changing the src
+    themeImage.onload = () => {
+      themeImage.style.opacity = 1;
+    };
+  }, 1000); // Match this with the transition duration (0.5s)
+});
+
+
 // script.js
 document.addEventListener('DOMContentLoaded', () => {
     const progressBars = document.querySelectorAll('.progress-bar');
@@ -19,5 +44,3 @@ document.addEventListener('DOMContentLoaded', () => {
     window.addEventListener('scroll', handleScroll);
     window.addEventListener('load', handleScroll);
 });
-
-
